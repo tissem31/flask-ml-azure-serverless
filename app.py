a3cc55a239a8a0270d7a0bb02cc1f66f2c3832c7
tissem31 <i.seghier@outlook.fr>
2025-04-19 09:46:50 +0000

    update version of app

--- app.py
+++ app.py
@@ -4,77 +4,44 @@ import logging
 import traceback
 
 import pandas as pd
-from sklearn.externals import joblib
+import joblib  # Changed import to directly import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
-
 def scale(payload):
     """Scales Payload"""
-
-    LOG.info("Scaling Payload: %s payload")
+    LOG.info("Scaling Payload: %s", payload)
     scaler = StandardScaler().fit(payload)
     scaled_adhoc_predict = scaler.transform(payload)
     return scaled_adhoc_predict
-
 
 @app.route("/")
 def home():
     html = (
         "<h3>Sklearn Prediction Home: From Azure Pipelines (Continuous Delivery)</h3>"
     )
-    return html.format(format)
+    return html
 
-
-# TO DO:  Log out the prediction value
 @app.route("/predict", methods=["POST"])
 def predict():
-    """Performs an sklearn prediction
-
-    input looks like:
-            {
-    "CHAS":{
-      "0":0
-    },
-    "RM":{
-      "0":6.575
-    },
-    "TAX":{
-      "0":296.0
-    },
-    "PTRATIO":{
-       "0":15.3
-    },
-    "B":{
-       "0":396.9
-    },
-    "LSTAT":{
-       "0":4.98
-    }
-
-    result looks like:
-    { "prediction": [ 20.35373177134412 ] }
-
-    """
-
+    """Performs an sklearn prediction"""
     try:
         clf = joblib.load("boston_housing_prediction.joblib")
     except Exception as e:
         LOG.error("Error loading model: %s", str(e))
         LOG.error("Exception traceback: %s", traceback.format_exc())
-    return "Model not loaded"
+        return jsonify({"error": "Model not loaded"}), 500  # Return error response if model fails to load
 
     json_payload = request.json
-    LOG.info("JSON payload: %s json_payload")
+    LOG.info("JSON payload: %s", json_payload)
     inference_payload = pd.DataFrame(json_payload)
-    LOG.info("inference payload DataFrame: %s inference_payload")
+    LOG.info("inference payload DataFrame: %s", inference_payload)
     scaled_payload = scale(inference_payload)
     prediction = list(clf.predict(scaled_payload))
     return jsonify({"prediction": prediction})
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
